2a39a630794a46d7fc2c20315ac5bb4ef4aadcce
thsergitox <sergio.pezo.j@uni.pe>
2023-12-28 21:49:40 -0500

    updt: "Stops were added"

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Polyline, ZoomControl } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Polyline, ZoomControl, Circle } from 'react-leaflet';
 import { SocketContext } from '../externalSocket/SocketContext';
 import L from 'leaflet';
 import customIconUrl from '../assets/bus.svg';
@@ -49,9 +49,6 @@ const MyMap = ({ item }) => {
     return () => {
       setPaths([])
       setPositions({})
-      if (socket) {
-        socket.off('actualizacionCoordenadas');
-      }
     };
   }, [socket, route]);
 
@@ -65,14 +62,16 @@ const MyMap = ({ item }) => {
         attribution='&copy; Made By Sergitox and Luisitox'
       />
       <ZoomControl position="topright" />
-
+ 
       {Object.entries(positions)
         .filter(([index, _]) => route.includes(index))
         .map(([index, pos]) => (
-          <Marker key={index} position={pos} icon={customIcon}>
+          <Marker key={index} position={pos} icon={customIcon} >
             <Popup>
-              Ruta: {index}<br />
-              Coordenadas: {pos}
+              <div style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '1.1em', marginBottom: '5px'}} >
+                Ruta: {index}
+              </div>
+              
             </Popup>
           </Marker>
         ))
@@ -82,16 +81,45 @@ const MyMap = ({ item }) => {
         <>
         <Polyline key={`outward-${index}`} positions={route.outwardPath} color={route.color}>
           <Popup>
-            Ruta: {route.routeid}<br />
-            Camino ida.
+          <div style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '1.1em', marginBottom: '5px'}} >
+              Ruta: {route.routeid}<br />
+              Camino ida.
+            </div>
           </Popup>
         </Polyline>
         <Polyline key={`return-${index}`} positions={route.returnPath} color={route.color}>
           <Popup>
-            Ruta: {route.routeid}<br />
-            Camino vuelta.
+            <div style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '1.1em', marginBottom: '5px'}} >
+              Ruta: {route.routeid}<br />
+              Camino vuelta.
+            </div>
           </Popup>
         </Polyline>
+
+        {route.outwardStops[1].map((pos, index) => {
+          return (
+            <Circle key={`outward-stops-${index}-${pos}`} radius={20} center={pos} color={route.color}>
+              <Popup>
+              <div style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '1.1em', marginBottom: '5px'}} >
+                Paradero: {route.outwardStops[0][index]}
+              </div>
+              </Popup>
+            </Circle>
+          )
+        })}
+
+        {route.returnStops[1].map((pos, index) => {
+          return (
+            <Circle key={`return-stops-${index}-${pos}`} radius={20} center={pos} color={route.color}>
+              <Popup>
+              <div style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '1.1em', marginBottom: '5px'}} >
+                Paradero: {route.returnStops[0][index]}
+              </div>
+              </Popup>
+            </Circle>
+          )
+        })}
+        
       </>
       ))}
     </MapContainer>
